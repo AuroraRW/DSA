9870b411df6e9620c760d9dfb59200abb5a0b6d9
AuroraRW <aurorawangca@gmail.com>
2024-09-12 22:28:38 -0600

    add Array and Hash Table

---
+++ Array/script.js
@@ -0,0 +1,22 @@
+// Array
+const arr= ['a', 'b', 'c', 'd'];
+const numbers = [1,2,3,4,5];
+
+
+//push
+arr.push('e'); // O(1)
+
+//pop
+arr.pop(); // O(1)
+arr.pop();
+
+//unshift
+arr.unshift('x') // O(n)
+
+//splice, add or remove
+arr.splice(2, 0, 'alien'); // O(n)
+
+console.log(arr)
+
+// C++
+// int a[20]
