c0421dfbef90d961209c7d91a99d137eb78fdf7e
AuroraRW <aurorawangca@gmail.com>
2024-09-19 21:36:09 -0600

    add stack and queue

---
+++ StacksAndQueues/StacksArray.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor(){
+      this.array = [];
+    }
+    peek() {
+      return this.array[this.array.length-1];
+    }
+    push(value){
+      this.array.push(value);
+      return this;
+    }
+    pop(){
+      this.array.pop();
+      return this;
+    }
+  }
+  
+  const myStack = new Stack();
+  myStack.peek();
+  myStack.push('google');
+  myStack.push('Youtube');
+  myStack.push('Facebook');
+  myStack.peek();
+  myStack.pop();
+  myStack.pop();
+  myStack.pop();
+  
+
+  
